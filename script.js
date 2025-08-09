75c26b9152adc58615d61f6e2b0d6c8a26b0b586
Tushar Minche <89399696+TusharMinche@users.noreply.github.com>
2025-08-09 11:22:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+
+codes[0].focus();
+
+codes.forEach((code, idx) => {
+  code.addEventListener("input", (e) => {
+    if (e.target.value.length === 1 && idx < codes.length - 1) {
+      codes[idx + 1].focus();
+    }
+  });
+
+  code.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (e.target.value === "" && idx > 0) {
+        codes[idx - 1].focus();
+      }
+    }
+  });
+});
